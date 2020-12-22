faa5d3549941586e3f94dd8eb2bd1228f18d52d1
Kareem <kareem.dare319@gmail.com>
2020-12-22 14:47:21 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const menu = document.getElementById('nav-wrapper');
+const menuIcon = document.querySelector('.hamburger');
+const navList = document.querySelector('.main-nav');
+
+
+
+menu.style.maxHeight = "0px";
+
+menuIcon.addEventListener('click', displayMenu);
+
+function displayMenu(){
+    if(menu.style.maxHeight == "0px"){
+       menu.style.maxHeight = "400px";
+    }
+    else{
+        menu.style.maxHeight = "0px";
+    }
+}
